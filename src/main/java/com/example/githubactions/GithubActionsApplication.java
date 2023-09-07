70775957f64fb76d7ffe2e1364e17f7e2430a021
karticchaudhary <karticchaudhary@gmail.com>
2023-09-08 04:08:30 +0530

    first commit

---
+++ src/main/java/com/example/githubactions/GithubActionsApplication.java
@@ -0,0 +1,21 @@
+package com.example.githubactions;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class GithubActionsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GithubActionsApplication.class, args);
+	}
+
+	@GetMapping("/build")
+	public String buildSuccess(){
+		return "BUILD SUCCESS";
+	}
+
+}
